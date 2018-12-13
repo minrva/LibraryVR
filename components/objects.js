b3ec08621d28ac6cfbde9ea2a96227ed9079ec79
John Diffor <jwdiffor@gmail.com>
2018-12-13 15:47:28 -0600

    First commit

---
+++ components/objects.js
@@ -0,0 +1,292 @@
+var img_4884 = {img:"/assets/photos/PHOTO_4884.jpg"};
+var img_4885 = {img:"/assets/photos/PHOTO_4885.jpg"};
+var img_4886 = {img:"/assets/photos/PHOTO_4886.jpg"};
+var img_4887 = {img:"/assets/photos/PHOTO_4887.jpg"};
+var img_4888 = {img:"/assets/photos/PHOTO_4888.jpg"};
+var img_4889 = {img:"/assets/photos/PHOTO_4889.jpg"};
+var img_4890 = {img:"/assets/photos/PHOTO_4890.jpg"};
+var img_4891 = {img:"/assets/photos/PHOTO_4891.jpg"};
+var img_4892 = {img:"/assets/photos/PHOTO_4892.jpg"};
+var img_4893 = {img:"/assets/photos/PHOTO_4893.jpg"};
+var img_4894 = {img:"/assets/photos/PHOTO_4894.jpg"};
+var img_4895 = {img:"/assets/rotated/PHOTO_4895.jpg"};
+var img_4896 = {img:"/assets/photos/PHOTO_4896.jpg"};
+var img_4897 = {img:"/assets/rotated/PHOTO_4897.jpg"};
+var img_4899 = {img:"/assets/rotated/PHOTO_4899.jpg"};
+var img_4900 = {img:"/assets/rotated/PHOTO_4900.jpg"};
+var img_4901 = {img:"/assets/photos/PHOTO_4901.jpg"};
+var img_4902 = {img:"/assets/photos/PHOTO_4902.jpg"};
+var img_4903 = {img:"/assets/rotated/PHOTO_4903.jpg"};
+var img_4904 = {img:"/assets/rotated/PHOTO_4904.jpg"};
+var img_4905 = {img:"/assets/rotated/PHOTO_4905.jpg"};
+var img_4906 = {img:"/assets/rotated/PHOTO_4906.jpg"};
+var img_4907 = {img:"/assets/rotated/PHOTO_4907.jpg"};
+var img_4908 = {img:"/assets/rotated/PHOTO_4908.jpg"};
+var img_4909 = {img:"/assets/rotated/PHOTO_4909.jpg"};
+var img_4910 = {img:"/assets/rotated/PHOTO_4910.jpg"};
+var img_4911 = {img:"/assets/rotated/PHOTO_4911.jpg"};
+var img_4912 = {img:"/assets/rotated/PHOTO_4912.jpg"};
+var img_4913 = {img:"/assets/rotated/PHOTO_4913.jpg"};
+var img_4914 = {img:"/assets/photos/PHOTO_4914.jpg"};
+
+var img_4915 = {img:"/assets/photos/PHOTO_4915.jpg"};
+var img_4916 = {img:"/assets/rotated/PHOTO_4916.jpg"};
+var img_4917 = {img:"/assets/rotated/PHOTO_4917.jpg"};
+
+
+var img_4385 = {img:"/assets/rotated/PHOTO_4385.jpg"};
+var img_4386 = {img:"/assets/rotated/PHOTO_4386.jpg"};
+var img_4387 = {img:"/assets/rotated/PHOTO_4387.jpg"};
+var img_4388 = {img:"/assets/rotated/PHOTO_4388.jpg"};
+var img_4390 = {img:"/assets/rotated/PHOTO_4390.jpg"};
+var img_4391 = {img:"/assets/rotated/PHOTO_4391.jpg"};
+var img_4392 = {img:"/assets/rotated/PHOTO_4392.jpg"};
+var img_4393 = {img:"/assets/rotated/PHOTO_4393.jpg"};
+var img_4394 = {img:"/assets/rotated/PHOTO_4394.jpg"};
+var img_4396 = {img:"/assets/rotated/PHOTO_4396.jpg"};
+var img_4397 = {img:"/assets/rotated/PHOTO_4397.jpg"};
+var img_4398 = {img:"/assets/rotated/PHOTO_4398.jpg"};
+var img_4399 = {img:"/assets/rotated/PHOTO_4399.jpg"};
+var img_4400 = {img:"/assets/rotated/PHOTO_4400.jpg"};
+var img_4401 = {img:"/assets/rotated/PHOTO_4401.jpg"};
+
+img_4884.right = img_4885;
+img_4884.left = null;
+img_4884.backward = null;
+img_4884.forward = null;
+
+img_4885.right = img_4886;
+img_4885.left = img_4884;
+img_4885.backward = null;
+img_4885.forward = null;
+
+img_4886.right = img_4888;
+img_4886.left = img_4885;
+img_4886.backward = null;
+img_4886.forward = null;
+
+
+img_4888.right = img_4889;
+img_4888.left = img_4886;
+img_4888.backward = null;
+img_4888.forward = null;
+
+img_4889.right = img_4890;
+img_4889.left = img_4888;
+img_4889.forward = null;
+img_4889.backward = null;
+
+img_4890.left = img_4889;
+img_4890.right = img_4891;
+img_4890.backward = null;
+img_4890.forward = null;
+
+img_4891.left = img_4890;
+img_4891.right = img_4892;
+img_4891.backward = null;
+img_4891.forward = null;
+
+img_4892.left = img_4891;
+img_4892.right = img_4893;
+img_4892.backward = null;
+img_4892.forward = null;
+
+img_4893.left = img_4892;
+img_4893.right = img_4894;
+img_4893.backward = null;
+img_4893.forward = null;
+
+img_4894.left = img_4893;
+img_4894.right = null;
+img_4894.backward = null;
+img_4894.forward = img_4895;
+
+img_4895.left = null;
+img_4895.right = img_4896;
+img_4895.backward = img_4894;
+img_4895.forward = img_4385;
+
+img_4896.left = img_4895;
+img_4896.right = img_4897;
+img_4896.backward = null;
+img_4896.forward = null;
+
+img_4897.left = img_4896;
+img_4897.right = null;
+img_4897.backward = null;
+img_4897.forward = img_4899;
+
+img_4899.left = null;
+img_4899.right = null;
+img_4899.backward = img_4897;
+img_4899.forward = img_4900;
+
+img_4900.left = img_4917;
+img_4900.right = img_4901;
+img_4900.backward = img_4899;
+img_4900.forward = img_4904;
+
+img_4901.left = img_4900;
+img_4901.right = img_4902;
+img_4901.backward = null;
+img_4901.forward = null;
+
+img_4902.left = img_4901;
+img_4902.right = img_4903;
+img_4902.backward = null;
+img_4902.forward = null;
+
+img_4903.left = img_4902;
+img_4903.right = null;
+img_4903.backward = null;
+img_4903.forward = null;
+
+img_4904.left = null;
+img_4904.right = null;
+img_4904.backward = img_4900;
+img_4904.forward = img_4905;
+
+img_4905.left = img_4916;
+img_4905.right = null;
+img_4905.backward = img_4904;
+img_4905.forward = img_4906;
+
+img_4906.left = null;
+img_4906.right = null;
+img_4906.backward = img_4905;
+img_4906.forward = img_4907;
+
+img_4907.left = null;
+img_4907.right = null;
+img_4907.backward = img_4906;
+img_4907.forward = img_4908;
+
+img_4908.left = img_4915;
+img_4908.right = null;
+img_4908.backward = img_4907;
+img_4908.forward = img_4909;
+
+img_4909.left = null;
+img_4909.right = null;
+img_4909.backward = img_4908;
+img_4909.forward = img_4910;
+
+img_4910.left = img_4913;
+img_4910.right = null;
+img_4910.backward = img_4909;
+img_4910.forward = img_4911;
+
+img_4911.left = null;
+img_4911.right = null;
+img_4911.backward = img_4910;
+img_4911.forward = img_4912;
+
+img_4912.left = null;
+img_4912.right = img_4914;
+img_4912.backward = img_4911;
+img_4912.forward = null;
+
+img_4913.left = img_4396;
+img_4913.right = img_4910;
+img_4913.backward = null;
+img_4913.forward = null;
+
+img_4914.left = img_4912;
+img_4914.right = null;
+img_4914.backward = null;
+img_4914.forward = null;
+
+img_4915.left = img_4392;
+img_4915.right = img_4908;
+img_4915.backward = null;
+img_4915.forward = null;
+
+img_4916.left = img_4390;
+img_4916.right = img_4905;
+img_4916.backward = null;
+img_4916.forward = null;
+
+img_4917.left = img_4386;
+img_4917.right = img_4900;
+img_4917.backward = null;
+img_4917.forward = null;
+
+img_4385.left = null;
+img_4385.right = null;
+img_4385.backward = img_4895;
+img_4385.forward = img_4386;
+
+img_4386.left = null;
+img_4386.right = img_4917;
+img_4386.backward = img_4385;
+img_4386.forward = img_4387;
+
+img_4387.left = null;
+img_4387.right = null;
+img_4387.backward = img_4386;
+img_4387.forward = img_4388;
+
+img_4388.left = null;
+img_4388.right = null;
+img_4388.backward = img_4387;
+img_4388.forward = img_4390;
+
+img_4390.left = null;
+img_4390.right = img_4916;
+img_4390.backward = img_4388;
+img_4390.forward = img_4391;
+
+img_4391.left = null;
+img_4391.right = null;
+img_4391.backward = img_4390;
+img_4391.forward = img_4392;
+
+img_4392.left = null;
+img_4392.right = img_4915;
+img_4392.backward = img_4391;
+img_4392.forward = img_4393;
+
+img_4393.left = null;
+img_4393.right = null;
+img_4393.backward = img_4392;
+img_4393.forward = img_4394;
+
+img_4394.left = null;
+img_4394.right = null;
+img_4394.backward = img_4393;
+img_4394.forward = img_4396;
+
+img_4396.left = img_4397;
+img_4396.right = img_4913;
+img_4396.backward = img_4394;
+img_4396.forward = null;
+
+img_4397.left = img_4398;
+img_4397.right = img_4396;
+img_4397.backward = null;
+img_4397.forward = null;
+
+img_4398.left = img_4399;
+img_4398.right = img_4397;
+img_4398.backward = null;
+img_4398.forward = null;
+
+img_4399.left = img_4400;
+img_4399.right = img_4398;
+img_4399.backward = null;
+img_4399.forward = null;
+
+img_4400.left = img_4401;
+img_4400.right = img_4399;
+img_4400.backward = null;
+img_4400.forward = null;
+
+img_4401.left = null;
+img_4401.right = img_4400;
+img_4401.backward = null;
+img_4401.forward = null;
+
+
+var start = img_4889;
+
+var current = start;
---
+++ components/set-image.js
@@ -0,0 +1,100 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 800}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function () {
+        if(current[data.src.split('-')[1]] != null) {
+
+            // Fade out image.
+            //document.querySelector('#image-current').emit('transition');
+            //document.querySelector('#image-' + data.src.split('-')[1]).emit('transition');
+            data.target.emit('set-image-fade');
+            console.log(data);
+            console.log(el);
+            // Wait for fade to complete.
+            setTimeout(function () {
+            // Set image.
+                data.target.setAttribute('material', 'src', current[data.src.split('-')[1]].img);
+                //data.target.setAttribute('material', 'opacity', 1);
+                //data.target.setAttribute('visible', false);
+
+                //
+                // document.querySelector('#image-' + data.src.split('-')[1]).setAttribute('visible', false);
+                // document.querySelector('#image-' + data.src.split('-')[1]).setAttribute('material', 'opacity', 0);
+
+                var previous = current;
+                current = current[data.src.split('-')[1]];
+
+                if(current.right == null) {
+                    document.querySelector('#goRightLink').setAttribute("visible", false);
+                    //document.querySelector('#right-image').setAttribute("src", "assets/arrow.png");
+                    //console.log(document.querySelector('#image-right'));
+                } else {
+                    document.getElementById('goRightLink').setAttribute("visible", true);
+                    //document.querySelector('#right-image').setAttribute("src", current['right'].img);
+                    //console.log(document.querySelector('#image-right'));
+                }
+                if(current.left == null) {
+                    document.getElementById('goLeftLink').setAttribute("visible", false);
+                    //document.querySelector('#image-left').setAttribute("material", "src", "assets/arrow.png");
+                } else {
+                    document.getElementById('goLeftLink').setAttribute("visible", true);
+                    //document.querySelector('#image-left').setAttribute("material", "src", current['left'].img);
+                }
+                if(current.forward == null) {
+                    document.getElementById('goForwardLink').setAttribute("visible", false);
+                    //document.querySelector('#image-forward').setAttribute("material", "src", "assets/arrow.png");
+                } else {
+                    document.getElementById('goForwardLink').setAttribute("visible", true);
+                    //document.querySelector('#image-forward').setAttribute("material", "src", current['forward'].img);
+                }
+                if(current.backward == null) {
+                    document.getElementById('goBackwardLink').setAttribute("visible", false);
+                    //document.querySelector('#image-backward').setAttribute("material", "src", "assets/arrow.png");
+                } else {
+                    document.getElementById('goBackwardLink').setAttribute("visible", true);
+                    //document.querySelector('#image-backward').setAttribute("material", "src", current['backward'].img);
+                }
+            }, data.dur/3);
+        }
+    });
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  setupFadeAnimation: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#888'
+    });
+  }
+});
